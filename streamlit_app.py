c27ba74ee42528479dcdd3f073fb65fff7474282
Hojin <HojinJava@gmail.com>
2026-04-01 15:05:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,13 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+pd_df = my_dataframe.to_pands()
+st.dataframe(pd_df)
+st.stop()
+
+search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
@@ -44,6 +51,12 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
     #st.write(ingredients_string)
+
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://www.smoothiefroot.com/api/fruit/" + fruit_chosen)
+    
 
     my_insert_stmt = """ insert into smoothies.public.orders (ingredients, name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
